f48b27e74860c0efdc2d03d5c28bca19e3056263
Ercin Akcay <ercin.akcay@sahibinden.com>
2018-12-31 00:09:14 +0300

    .

--- sb-rx-web/src/main/java/com/ea/sbrxweb/service/BaseService.java
+++
@@ -1,9 +0,0 @@
-package com.ea.sbrxweb.service;
-
-import io.reactivex.Observable;
-
-public abstract class BaseService extends AbstractRestService
-{
-//    public abstract Observable<String> getDestinations(String from);
-//    public abstract Observable<FlightDTO> getFlights(String from, String to);
-}
