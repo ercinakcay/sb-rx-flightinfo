d2f0e0af764bac9facacafc3730e18719c0108b5
Ercin Akcay <ercin.akcay@sahibinden.com>
2019-01-02 09:01:50 +0300

    .

--- sb-rx-web/src/main/java/com/ea/sbrxweb/controller/StarterAPI.java
+++ sb-rx-web/src/main/java/com/ea/sbrxweb/controller/StarterAPI.java
@@ -2,48 +2,18 @@ package com.ea.sbrxweb.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ea.sbrxweb.dto.AirportDTO;
-import com.ea.sbrxweb.service.AirplaneService;
-
-import io.reactivex.Observable;
 
 @Controller
 @RequestMapping("/")
 public class StarterAPI extends BaseController
 {
-    @Autowired
-    private AirplaneService africaAirplaneService;
-
-    @Autowired
-    private AirplaneService antarcticaAirplaneService;
-
-    @Autowired
-    private AirplaneService asiaAirplaneService;
-
-    @Autowired
-    private AirplaneService europeAirplaneService;
-
-    @Autowired
-    private AirplaneService northAmericaAirplaneService;
-
-    @Autowired
-    private AirplaneService oceaniaAirplaneService;
-
-    @Autowired
-    private AirplaneService southAmericaAirplaneService;
 
     @RequestMapping("/")
-    public String home(Map<String, Object> model, @PathVariable String from)
+    public String home(Map<String, Object> model)
     {
-        Observable<AirportDTO> result = europeAirplaneService.getDestinationList();
-
-        model.put("message", "docker is ready to start!" + result.subscribe(System.out::println).toString());
+        model.put("message", "docker is ready to start!");
         return "index";
     }
 
